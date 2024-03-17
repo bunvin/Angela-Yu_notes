dbd6851d9fd19600d45708707e05e0a757bbf9db
NaamaL <bunvin@gmail.com>
2024-03-17 10:55:03 +0200

    testing

--- My projects/Blog/index.js
+++ My projects/Blog/index.js
@@ -9,15 +9,25 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    pos1_title: post1.title,
-    pos1_post: post1.post,
-    pos1_timePost: post1.timePosted,
-    pos2_title: post2.title,
-    pos2_post: post2.post,
-    pos2_timePost: post2.timePosted,}
-    ) 
-  });
+  const article = [{ 
+    title: "orc meeting 3",
+    post: "the meeting was so interesting to watch, I'm glad I spied on them. the meeting was so interesting to watch, I'm glad I spied on them",
+    timePosted: new Date(2020,10,29,10,33).toDateString()
+  },
+  {
+    title: "orc meeting 2",
+    post: "Lorem ipsum dolor sit amet, quis in duis, iaculis id felis. Consectetuer vestibulum, nunc urna lectus, erat ligula. Hendrerit nam, lectus ante, ut lorem eros.",
+    timePosted: new Date(2019,10,29,10,33).toDateString()
+},
+
+{
+  title: "orc meeting 1",
+  post: "Lorem ipsum dolor sit amet, quis in duis, iaculis id felis. Consectetuer vestibulum, nunc urna lectus, erat ligula. Hendrerit nam, lectus ante, ut lorem eros.",
+  timePosted: new Date(2018,10,29,10,33).toDateString()
+},
+]
+  res.render("index.ejs", {articles: article }) 
+});
 
 app.post("/add", (req, res) => {
     res.render("add.ejs");
